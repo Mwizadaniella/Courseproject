7fcb3bb46742f69fc300f69e0b439dd50ef0074d
Mwizadaniella <133741664+Mwizadaniella@users.noreply.github.com>
2023-07-14 03:10:01 -0700

    projectwork
    mwiza

---
+++ projectwork.py
@@ -0,0 +1,67 @@
+//function to get the name of the user
+function getNameOfTheUser(){
+//ask the user for first name and last name 
+//show the user their names after filling in their names 
+//save the user's names in the sub program
+}
+
+
+//function for the user to create a password
+function createAPassword(){
+//ask the user to fill in a password 
+//ask the user to confirm the password the user entered
+//save the user's password in the sub program
+}
+
+
+//function to provide financial education
+function provideFinancialEducation(){
+    displayMessage("We welcome you to the Financial Education Program.")
+	displayMessage("Let the Financial Education Program teach you about banking services, budgeting, saving, and more.")
+    
+if the user is financially educated
+   print("Proceed to reviewAndUpdateDocumentation.")  
+else   
+   print("Continue to provideFinancialEducation.")
+   
+//ask user for the language they prefer to use
+//show educational content and resources in the user's chosen language
+//provide quizzes to test users on what they have learnt
+//provide corrections to the questions failed
+//offer certificates or rewards for completion
+}
+
+
+//function to review and update documentation
+function reviewAndUpdateDocumentation(){
+    displayMessage("We welcome you to the Documentation Review and Update procedure.")
+//ask the user which language they would like to use
+//show the rest of the procedure in the chosen language
+    displayMessage("Identify outdated or incomplete documentation and update accordingly.")
+    
+if the user already has updated information
+    print("Proceed to provideBudgetingAssistance")
+else
+    print("Continue with reviewAndUpdateDocumentation")
+    
+//retrieve the existing documentation
+//perform an audit to identify outdated information
+//update the documentation with accurate and comprehensive content	
+}
+
+
+//function to provide budgeting assistance
+function provideBudgetingAssistance(){
+    displayMessage("We welcome you to Budgeting Assistance.")
+	displayMessage("Helping you with the creation and management of your budget effectively.")
+//ask the user which language they would like to use
+//proceed with preferred language
+//collect user's income and expenses information
+//deduct expenses from income 
+//calculate the available funds
+//provide recommendations on budget allocations and saving strategies	
+}
+//end
+function end(){
+    displayMessage("Congratulations, you have completed all the stages successfully.") 
+}  
